6f20b1cd2693014d2e4a48002aa6298804263047
Sashwat-43 <mishrashashwat43@gmail.com>
2022-04-08 18:14:09 +0530

    added Top NavBar

--- socialmediafrontend/src/components/TopNavBar/TopNavBar.jsx
+++ socialmediafrontend/src/components/TopNavBar/TopNavBar.jsx
@@ -1,31 +1,40 @@
 import React from 'react'
 import './TopNavBar.css'
-import {Person,Search} from '@mui/icons-material';
+import {Person,Search,Notifications,Chat} from '@mui/icons-material';
 
 
 export default function TopNavBar() {
   return (
     <div className='TopNavBarContainer'>
         <div className='TopNavBarLeft'>
-            <span className='icon'>Social</span>
+            <span className='icon'>Socialgram</span>
         </div>
         <div className='TopNavBarCenter'>
             <div className= 'Search'>
-                <Search/>
+                <Search className='SearchLogo'/>
                 <input className='SearchInput' placeholder='Search for post/video/friend'></input>
             </div>
         </div>
         <div className='TopNavBarRight'>
             <div className='TopNavBarLinks'>
-                <span className='Link1'>HomePage</span>
-                <span className='Link2'>All Posts</span>
+                <span className='Link1'>Home</span>
+                <span className='Link2'>Posts</span>
             </div>
             <div className='TopNavBarIcons'>
                 <div className='TopNavBarIcon'>
                     <Person />
                     <span className='TopNavBarIconCount'>2</span>
                 </div>
+                <div className='TopNavBarIcon'>
+                    <Chat />
+                    <span className='TopNavBarIconCount'>2</span>
+                </div>
+                <div className='TopNavBarIcon'>
+                    <Notifications /> 
+                    <span className='TopNavBarIconCount'>2</span>
+                </div>
             </div>
+            <img className='TopNavBarImage' src='/assets/profilePics/pic1.png' alt="image1"></img>
         </div>
     </div>
   )
