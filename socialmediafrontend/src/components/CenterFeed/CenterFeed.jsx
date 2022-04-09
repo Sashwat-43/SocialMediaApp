129b9313347b2a83af8124ffca5f4ccc8c1eb68d
Sashwat-43 <mishrashashwat43@gmail.com>
2022-04-09 14:29:29 +0530

    added center post feed

--- socialmediafrontend/src/components/CenterFeed/CenterFeed.jsx
+++ socialmediafrontend/src/components/CenterFeed/CenterFeed.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './CenterFeed.css'
 import SharePost from '../SharePost/SharePost.jsx';
+import Post from '../Post/Post.jsx';
 
 export default function CenterFeed() {
   return (
     <div className='CenterFeed'>
       <div className='CenterWrap'>
         <SharePost/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
       </div>
     </div>
   )
--- socialmediafrontend/src/components/Post/Post.jsx
+++ socialmediafrontend/src/components/Post/Post.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
 import './Post.css';
+import {MoreVert} from '@mui/icons-material';
 
 export default function Post() {
   return (
-    <div>Post</div>
+    <div className='Post'>
+      <div className='PostWrap'>
+        <div className='PostTop'>
+          <div className='PostTopLeft'>
+            <img src='/assets/ProfilePics/pic2.png' alt='Post1' className='PostTopLeftImage'></img>
+            <span className='PostTopName'>Sashwat Mishra</span>
+            <span className='PostTopDate'>5 mins ago</span>
+          </div>
+          <div className='PostTopRight'>
+            <MoreVert className='Vertical'/>
+          </div>
+        </div>
+        <div className='PostMiddle'>
+          <span className='PostMiddleText'>Sashwat's post</span>
+          <img src='/assets/Posts/pic2.png' alt='Post' className='PostMiddleImage'></img>
+        </div>
+        <div className='PostBottom'>
+          <div className='PostBottomLeft'>
+            <img src='/assets/Usecase/like.png' alt='like' className='PostBottomLike'></img>
+            <img src='/assets/Usecase/love.png' alt='love' className='PostBottomLove'></img>
+            <span className='PostLikes'>20 people reacted</span>
+          </div>
+          <div className='PostBottomRight'>
+            <span className='PostComments'>6 comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
