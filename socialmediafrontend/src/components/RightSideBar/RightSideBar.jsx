376b540df248cd63bf82e10a26d0e66f5596e18b
Sashwat-43 <mishrashashwat43@gmail.com>
2022-04-10 17:50:46 +0530

    added Right SideBar

--- socialmediafrontend/src/components/RightSideBar/RightSideBar.jsx
+++ socialmediafrontend/src/components/RightSideBar/RightSideBar.jsx
@@ -3,6 +3,46 @@ import './RightSideBar.css';
 
 export default function RightSideBar() {
   return (
-    <div className='RightSideBar'>RightSideBar</div>
+    <div className='RightSideBar'>
+      <div className='RightSideBarWrap'>
+        <div className='Birthday'>
+          <img src='/assets/UseCase/birthday.png' alt='Hapy Birthday' className='BirthdayImage'></img>
+          <span className='BirthdayText'><b>Komal's</b> birthday today</span>
+          
+        </div>
+        <img src='/assets/UseCase/advertisement.png' alt='Advertisement' className='AdvertisementImage'></img>
+        <span className='RigthSideBarTitle'>Online Friends</span>
+        <ul className='OnlineFriends'>
+          <li className='OnlineFriend'>
+            <div className='OnlineFriendContainer'>
+              <img className='OnlineFriendImage' src='/assets/ProfilePics/pic1.png' alt="Friend1"></img>
+              <span className='Online'></span>
+            </div>
+            <div className='OnlineFriendName'>Sashwat</div>
+          </li>
+          <li className='OnlineFriend'>
+            <div className='OnlineFriendContainer'>
+              <img className='OnlineFriendImage' src='/assets/ProfilePics/pic1.png' alt="Friend1"></img>
+              <span className='Online'></span>
+            </div>
+            <div className='OnlineFriendName'>Sashwat</div>
+          </li>
+          <li className='OnlineFriend'>
+            <div className='OnlineFriendContainer'>
+              <img className='OnlineFriendImage' src='/assets/ProfilePics/pic1.png' alt="Friend1"></img>
+              <span className='Online'></span>
+            </div>
+            <div className='OnlineFriendName'>Sashwat</div>
+          </li>
+          <li className='OnlineFriend'>
+            <div className='OnlineFriendContainer'>
+              <img className='OnlineFriendImage' src='/assets/ProfilePics/pic1.png' alt="Friend1"></img>
+              <span className='Online'></span>
+            </div>
+            <div className='OnlineFriendName'>Sashwat</div>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
