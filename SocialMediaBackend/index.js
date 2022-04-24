23b5fddbb19c0f5117f985830c4430db3be436f7
Sashwat-43 <mishrashashwat43@gmail.com>
2022-04-24 07:20:18 +0530

    added multer for uploading image in the server side

--- SocialMediaBackend/index.js
+++ SocialMediaBackend/index.js
@@ -7,7 +7,7 @@ const app = express();
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
-
+const multer = require("multer");
 
 dotenv.config();
 
@@ -16,6 +16,25 @@ app.use(helmet());
 app.use(express.json());
 app.use(morgan("common"));
 
+const storage = multer.diskStorage({
+  destination: (req,file,cb)=>{
+    cb(null,"public/images");
+  },
+  filename: (req,file,cb) =>{
+    cb(null,file.originalname);
+  }
+});
+
+const upload = multer({storage});
+app.post('/api/uploadMedia', upload.single("file"),(req,res) => {
+  try{
+
+    return res.status(200).json("File uploaded!");
+
+  }catch(err){
+    console.log(err);
+  }
+})
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
--- socialmediafrontend/src/components/SharePost/SharePost.jsx
+++ socialmediafrontend/src/components/SharePost/SharePost.jsx
@@ -1,13 +1,34 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState , useRef } from 'react'
+import axios from 'axios';
 import './SharePost.css';
-import {PermMedia,EmojiEmotions,Label,Room} from "@mui/icons-material"
+import {PermMedia,EmojiEmotions,Label,Room, LineAxisOutlined} from "@mui/icons-material"
 import { Context } from '../../ContextApi/Context';
 
 export default function SharePost() {
 
   const {user} = useContext(Context);
   const publicFolder = process.env.REACT_APP_PUBLIC_FOLDER_IMAGES;
+  const bio = useRef(null);
+  const [file,setFile] = useState(null);
 
+  const handleMedia = (e) => {
+    setFile(e.target.files[0])
+  }
+
+  const handleShareSubmit = async (e) =>{
+    e.preventDefault();
+    const tempPost = {
+      userId: user._id,
+      bio: bio.current.value
+    }
+    try{
+
+      const response = await axios.post('/posts',tempPost);
+      console.log(response);
+    }catch(err){
+      console.log(err);
+    }
+  }
 
   return (
     <div className='SharePost'>
@@ -17,29 +38,32 @@ export default function SharePost() {
             <input className='SharePostInput' placeholder='What to write?'></input>
         </div>
         <hr className='SharePostLine'></hr>
-        <div className='SharePostBottom'>
-          <div className='SharePostOptions'>
-            <div className='Option'>
-              <PermMedia style={{ color: "red" }} className='SharePostIcon'/>
-                <span className='ShareText'>Media</span>
+        <form className='SharePostBottom' onSubmit={ handleShareSubmit}>
+          <div className='Container'>
+            <div className='SharePostOptions'>
+              <label htmlFor='file' className='Option'>
+                <PermMedia style={{ color: "red" }} className='SharePostIcon'/>
+                  <span className='ShareText'>Media</span>
+                  <input style={{display:"none"}}type="file" accept=".png .jpeg .jpg" id="file" onChange={handleMedia}/>
+              </label>
+              <div className='Option'>
+                <Room style={{ color: "green" }} className='SharePostIcon'/>
+                  <span className='ShareText'>Location</span>
+              </div>
+              <div className='Option'>
+                <Label style={{ color: "blue" }} className='SharePostIcon'/>
+                  <span className='ShareText'>Tag</span>
+              </div>
+              <div className='Option'>
+                <EmojiEmotions style={{ color: '#8B8000' }} className='SharePostIcon'/>
+                  <span className='ShareText'>Emotions</span>
+              </div>
+            </div>   
+            <button className='SharePostButton' type='submit'>
+              Share
+            </button>
             </div>
-            <div className='Option'>
-              <Room style={{ color: "green" }} className='SharePostIcon'/>
-                <span className='ShareText'>Location</span>
-            </div>
-            <div className='Option'>
-              <Label style={{ color: "blue" }} className='SharePostIcon'/>
-                <span className='ShareText'>Tag</span>
-            </div>
-            <div className='Option'>
-              <EmojiEmotions style={{ color: '#8B8000' }} className='SharePostIcon'/>
-                <span className='ShareText'>Emotions</span>
-            </div>
-          </div>
-        </div>
-        <button className='SharePostButton'>
-            Share
-        </button>
+        </form>
       </div>
     </div>
   )
