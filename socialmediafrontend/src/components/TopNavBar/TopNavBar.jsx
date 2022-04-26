05421e7b82a6dd0187ed34825b78105661bf430d
Sashwat-43 <mishrashashwat43@gmail.com>
2022-04-26 12:58:31 +0530

    added Search User in TopNavBar

--- socialmediafrontend/src/components/TopNavBar/TopNavBar.jsx
+++ socialmediafrontend/src/components/TopNavBar/TopNavBar.jsx
@@ -1,14 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import './TopNavBar.css'
-import {Person,Search,Notifications,Chat} from '@mui/icons-material';
-import {Link} from 'react-router-dom'
+import {Person,Search ,Notifications,Chat} from '@mui/icons-material';
+import {Link, Navigate, useNavigate} from 'react-router-dom'
 import {Context} from '../../ContextApi/Context';
 import Register from '../../pages/Auth/Register';
+import axios from 'axios';
 
 export default function TopNavBar() {
 
     const {user} = useContext(Context);
+    const searchUser = useRef();
+    const navigate = useNavigate();
     const publicFolder = process.env.REACT_APP_PUBLIC_FOLDER_IMAGES;
+
+    const handleSearch = async (e) =>{
+        e.preventDefault();
+        try{
+            const response = await axios.get(`/users?username=${searchUser.current.value}`);
+            navigate(`/MyProfile/${response.data.username}`)
+        }catch(err){
+            alert("No user exists with this username!");
+            console.log(err);
+        }
+
+    }
 
   return (
     user?
@@ -19,9 +34,11 @@ export default function TopNavBar() {
             </Link>
         </div>
         <div className='TopNavBarCenter'>
-            <div className= 'TopNavBarSearch'>
-                <Search className='SearchLogo'/>
-                <input className='SearchInput' placeholder='Search for post/video/friend'></input>
+            <div className= 'TopNavBarSearch' >
+                <form className='SearchUser' onSubmit={handleSearch}>
+                    <Search className='SearchLogo' type='submit'/>
+                    <input className='SearchInput' required ref={searchUser} placeholder='Search for friend'></input>
+                </form>
             </div>
         </div>
         <div className='TopNavBarRight'>
